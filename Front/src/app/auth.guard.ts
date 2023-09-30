2471da4d197d9335eaed22779118cc13be5aae4a
evasow <evasow2001@gmail.com>
2023-09-30 14:34:24 +0000

    login : updating

--- Front/src/app/auth.guard.ts
+++ Front/src/app/auth.guard.ts
@@ -3,9 +3,7 @@ import { CanActivateFn, Router } from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   let router = new Router
   const localToken=localStorage.getItem('login');
-  if (localToken) {
-    console.log(route, state);
-    
+  if (localToken) {    
     return true;
   }
   router.navigate(['']);
--- Front/src/app/produit-vente/produit-vente.component.ts
+++ Front/src/app/produit-vente/produit-vente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-produit-vente',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./produit-vente.component.css']
 })
 export class ProduitVenteComponent {
+
+
+  constructor(private fb:FormBuilder){}
+
+  formProduits=this.fb.group({
+    libelle : [""],
+    code : [{value: null,disabled: true}],
+    photo :  [""],
+    quantite_stock :  [null], 
+    prix_unitaire :  [null], 
+    marque : [""],
+    categorie : [""],
+    prix_gros :  [null], 
+    succursale_id :  [null], 
+    caracteristiques : this.fb.array([])
+  });
+
+  newCaracteristiques(){
+    return this.fb.group({
+      caracteristiques_id : [null],
+      valeur : [null],
+      unite_id : [null],
+    })
+  }
+
+  get caracteristiques():FormArray{
+      return this.formProduits.get("caracteristiques") as FormArray;
+  }
+
+  addCaracteristique(){
+    this.caracteristiques.push(this.newCaracteristiques());
+  }
+
+  removeCaracteristique(i:number){
+    this.caracteristiques.removeAt(i);
+  }
+
+  onSubmitProduct(){
+
+  }
+
 
 }
